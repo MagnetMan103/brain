f99c83b5dcb7f8b655d525416077ffbac0254d55
Alan Munschy <magnetman103@gmail.com>
2026-04-10 19:05:04 +0000

    added grabber

--- camera.py
+++ camera.py
@@ -24,14 +24,13 @@ class Vision():
         """
         self.cameraType = cameraType
         
-        # New robust camera initialization
         if self.cameraType == "usb":
-            # Bypass GStreamer and use native Linux V4L2
             self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
             self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
             self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-            # Force MJPEG format to prevent slow raw feeds
             self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+            # Attempt to set buffer size to 1 (Supported on some Linux drivers)
+            self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
         else:
             self.cap = cv2.VideoCapture(CSI_GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
 
@@ -47,20 +46,39 @@ class Vision():
         self.encoded_frame = None
         self._frame_event = threading.Event()
 
+        # --- Frame Capture Threading Variables ---
+        self._latest_raw_frame = None
+        self._latest_ret = False
+        self.camera_running = True
+        self.capture_thread = threading.Thread(target=self._capture_loop, daemon=True)
+        self.capture_thread.start()
+
         if self.stream:
             start_stream(self)
 
-    def get_frame(self):
-        """Returns the current capture of the camera and applies transforms"""
-        ret, frame = self.cap.read()
-        self.ret = ret
-        
-        if ret:
-            if self.cameraType == "usb":
-                # Rotate 180 degrees as specified in your new script
-                self.frame = cv2.flip(frame, -1)
+    def _capture_loop(self):
+        """Background thread that constantly pulls frames to empty the buffer."""
+        while self.camera_running:
+            if self.cap.isOpened():
+                ret, frame = self.cap.read()
+                self._latest_ret = ret
+                if ret:
+                    self._latest_raw_frame = frame
             else:
-                self.frame = frame
+                time.sleep(0.01)
+
+    def get_frame(self):
+        """Returns the most recent capture of the camera and applies transforms"""
+        self.ret = self._latest_ret
+        
+        if self.ret and self._latest_raw_frame is not None:
+            # Create a copy so we don't manipulate the frame while the thread updates it
+            frame_copy = self._latest_raw_frame.copy()
+            
+            if self.cameraType == "usb":
+                self.frame = cv2.flip(frame_copy, -1)
+            else:
+                self.frame = frame_copy
         else:
             self.frame = None
 
@@ -113,3 +131,9 @@ class Vision():
         self.detect_bottles()
         if self.stream and self.frame is not None:
             self.setup_overlay()
+            
+    def close(self):
+        """Safely shuts down the capture thread and releases hardware"""
+        self.camera_running = False
+        if self.cap.isOpened():
+            self.cap.release()
